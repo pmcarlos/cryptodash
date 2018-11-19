bab2dfb23c118d36cc2d9192af4c9644528140e1
pmcarlos <carlos@passaromx.com>
2018-11-19 17:44:34 -0600

    AppBar added

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import styled, {css} from 'styled-components'
 import AppLayout from './AppLayout'
+import AppBar from './AppBar'
+import WelcomeMessage from './WelcomeMessage';
 
 class App extends Component {
   render() {
     return (
       <AppLayout >
-        Hello world
+        <AppBar />
+        <WelcomeMessage />
       </AppLayout>
     );
   }
