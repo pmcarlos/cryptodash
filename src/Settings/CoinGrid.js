4c1d9a112fd1c2a3ab0beccca1d60607c03406f3
pmcarlos <carlos@passaromx.com>
2018-11-19 21:29:10 -0600

    Add TopSection CoinGrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -10,16 +10,16 @@ const CoinGridStyled = styled.div`
   grid-gap: 15px;
   margin-top: 40px;
 `
-function getCoinsToDisplay (coinList) {
-  return Object.keys(coinList).slice(0,100)
+function getCoinsToDisplay (coinList, topSection) {
+  return Object.keys(coinList).slice(0,topSection ? 10 : 100)
 }
-export default function() {
+export default function({topSection}) {
   return(
     <AppContext.Consumer>
       {({coinList}) => (
         <CoinGridStyled>
-          {getCoinsToDisplay(coinList).map(coinKey => (
-            <CoinTile key={coinKey} coinKey={coinKey} />
+          {getCoinsToDisplay(coinList, topSection).map(coinKey => (
+            <CoinTile topSection={topSection} key={coinKey} coinKey={coinKey} />
           ))}
         </CoinGridStyled>
       )}
